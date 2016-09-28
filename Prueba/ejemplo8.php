e75ec8f79d2efbf2b632d268498b79c89cc07e6a
BrandConstantin <brand_constantin@hotmail.com>
2016-09-28 14:25:17 +0200

    Terminando ejemplos

---
+++ Prueba/ejemplo8.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--
+adivinar número
+-->
+<html>
+    <head>
+        <title>Adivinar número</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        Adivina el número!
+        <hr style="border-style: dotted 1px #f34">
+        <?php
+            $numIntrod = $_POST["numIntrod"];
+            $oportunidades = $_POST["oportunidades"];
+            $numSecreto = 24;
+            
+            if($numIntrod == $numSecreto){
+                echo 'Enhorabuena has adivinado';
+            }else{
+                if($oportunidades == 0){
+                    echo 'Lo siento, se te han acabado las oportunidades';
+                }else{
+                    if($numIntrod != 555){
+                        if($numSecreto > $numIntrod){
+                            echo 'El número es mayor que el introducido';
+                        }else{
+                            echo 'El número es menor que el introducido';
+                        }
+                    }
+                    ?>
+                    Te quedan <?= $oportunidades-- ?> oportunidades. <br>
+                    Introduce otro número:
+                    <form action="ejemplo8.php" method="post">
+                        <input type="text" name="numIntrod">
+                        <input type="hidden" name="oportunidades" value="<?= $oportunidades ?>">
+                        <input type="submit" name="Continuar">
+                    </form>
+                    <?php
+                }
+            }
+        ?>
+    </body>
+</html>
