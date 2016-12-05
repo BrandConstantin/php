9fffc2d630c3c848608ffb60e3fe8ff339cd9d15
BrandConstantin <brand_constantin@hotmail.com>
2016-12-05 10:52:55 +0100

    Actualizando

--- Prueba/conexionBD.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conexion = mysql_connect("localhost", "root", "root");
-    
-    if($conexion){
-        echo "Se ha establecido conexión con el servidor de BD";
-    }else{
-        echo "No se ha podido establecer conexión con el servidor de BD";
-    }
-?>
